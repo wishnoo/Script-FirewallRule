f4af36ffb2be46f91bee07a56979c749448c4f70
Wishnoo Raghunadh <rwishnoo@gmail.com>
2021-07-07 11:55:00 -0400

    Added loop to delete existing old path rules

--- Script-FirewallRule.ps1
+++ Script-FirewallRule.ps1
@@ -1,8 +1,25 @@
+# Array to store all old locations in order to be processed in this case to be deleted from the firewall.
+$deprecatedPathLocationArray = @()
+# Variable to store the path string with the current location of the rule.
+$currentPathLocation = "C:\users\wishnoo\appdata\local\microsoft\teams\current\teams.exe"
+
+$action_value = "Block"
+
+# Loop through the Array to delete existing old path rules
+if (-not $deprecatedPathLocationArray.count -gt 0) {
+    foreach ($path in $deprecatedPathLocationArray) {
+        $applicationObject_ProgramItem = Get-NetFirewallApplicationFilter -Program $path
+        Remove-NetFirewallRule -InputObject $applicationObject_ProgramItem
+    }
+}
+
+# Remove firewall rules with appropriate action and current Path
 
 # Get-NetFirewallRule allows us to return instances of firewall rules that matches the action "Allow".
 # Get-NetFirewallApplicationFilter accepts AssociatedNetFirewallRule <CimInstance> through the pipeline (Byvalue) and it returns objects filter objects based on the input.
-$AllowApplicationobject = Get-NetFirewallRule -Action "Allow" | Get-NetFirewallApplicationFilter
+$applicationObject_Allow = Get-NetFirewallRule -Action $action_value | Get-NetFirewallApplicationFilter
 
 # Return the filter objects for the program path that team is currently in.
-$AllowApplicationobject | Where-Object {$_.Program -eq "C:\users\wishnoo\appdata\local\microsoft\teams\current\teams.exe"} | Format-List *
+$applicationObject_Program = $applicationobject_Allow | Where-Object {$_.Program -eq $currentPathLocation}
 
+$applicationObject_Program | Remove-NetFirewallRule
