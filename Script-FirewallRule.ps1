b5af1e8916de2695c1ad5e37b3695a109e2ada1e
Wishnoo Raghunadh <rwishnoo@gmail.com>
2021-07-08 12:39:59 -0400

    Better Commenting and squashed errors where the section skips the search result if not found instead of catching an error

--- Script-FirewallRule.ps1
+++ Script-FirewallRule.ps1
@@ -1,108 +1,157 @@
+## Overview:
+## This script is used to add rules to the firewall. The reason this is needed is because Microsoft is running Teams from the AppData folder of each user when it starts
+## a call.It also has dynamic sections that handles possible deprecated paths and blocked rules incorrectly created because of the user clicking cancel. 
 
-# This makes all non terminating errors stop
+
+## This makes all non terminating errors stop
 $ErrorActionPreference = 'Stop'
 
-# Array to store all old locations in order to be processed in this case to be deleted from the firewall.
+## This array is used to store Teams old file locations. For example, Teams is currently in Appdata if that is changed we will
+## not know what to delete. So, we store the old locations here in order to delete them. 
 $deprecatedPathLocationArray = @("AppData\Local\Microsoft\Teams\Current\Teams.exe")
-# Variable to store the path string with the current location of the rule.
+
+## Variable to store the path string with the current location of the rule.
 $currentPathLocation = ""
 
-# Action value depicts the action filter we can choose e.g.: "Allow","Block" 
+## Action value depicts the action filter we can choose e.g.: "Allow","Block" 
 $actionToDelete = "Block"
 
-#Action value required to add rules in the firewall
+## Action value required to add rules in the firewall
 $actionToAdd = "Allow"
 
 $users = Get-ChildItem (Join-Path -Path $env:SystemDrive -ChildPath 'Users') -Exclude 'Public', 'ADMINI~*'
 
 if ($null -ne $users) {
+
+
+## Section: Delete old paths for the program TEAMS
+## How it works:
+## Iterate through the progam field in the firewall in order to find deprecated paths.
+## Iterate through the users folder to create proper user paths.
+## Join current path with the users path
+## Use Get-NetFirewallApplicationFilter to find the rules with the created path.
+## Delete all the rules which we found
+
     try {
-        # Loop through the Array to delete existing old path rules
+        ## Loop through the Array to delete existing old path rules
         Write-Host "`n START: Section to delete old path rules `n"
         if ($deprecatedPathLocationArray) {
+            $counter = 0
             foreach ($path in $deprecatedPathLocationArray) {
-                Write-Host "Path in array : $($path)"
-                # Iterate through each user folder and remove rules with respect to the user regardless of the action
+                Write-Host "OUTPUT: Path in array : $($path)"
+                ## Iterate through each user folder and remove rules with respect to the user regardless of the action
                 foreach ($user in $users) {
-                    # Combine the user folder bath with path provided in the deprecated location array
+                    ## Combine the user folder path with path provided in the deprecated location array
                     $fullProgramPath = Join-Path -Path $user.FullName -ChildPath $path
-                    $applicationObject_ProgramItem = Get-NetFirewallApplicationFilter -Program $fullProgramPath
-                    $applicationObject_ProgramItem | Remove-NetFirewallRule
-                    Write-Host "Rule with path '$($fullProgramPath)' deleted"
+                    ## Use the ErrorAction Parameter to avoid catching the error and continue the search when no rules are found.
+                    ## The ErrorVariable parameter is used to store the error in a custom name field which could be later 
+                    $applicationObject_ProgramItem = Get-NetFirewallApplicationFilter -Program $fullProgramPath -ErrorAction SilentlyContinue -ErrorVariable oldPathSearchError
+                    If($applicationObject_ProgramItem){
+                        $applicationObject_ProgramItem | Remove-NetFirewallRule
+                        Write-Host "OUTPUT: Rule with path '$($fullProgramPath)' deleted"
+                        $counter = 1
+                    }
+                }
+            }
+            if ($counter -eq 0) {
+                Write-Host "OUTPUT: No Rule Deleted."
+            }
+             
+        }
+       
+        else {
+            Write-Host "OUTPUT: No Old Location Path to delete."
+        }
+
+    }
+    catch {
+        Write-Host "`n START: Catch Section to delete old path rules `n "
+        Write-Host "Exception.Message [$($Error)]"
+        Write-Host "`n END: Catch Section to delete old path rules `n"
+    }
+    finally{
+        Write-Host "`n END: Section to delete old path rules `n"
+    }
+    
+## Section: Delete the block rules
+## This is needed if a user clicks on the cancel button on the TEAMS popup becasue TEAMS creates block rules when this action is taken
+## and we need to remove these block rules. 
+## How it works:
+## Check the firewall rules to find the block rules.
+## Iterate through the progam field in the firewall in order to find deprecated paths.
+## Iterate through the users folder to create proper user paths.
+## Get all the firewall block rules with Get-NetFirewallRule and pass it as an argument to Get-NetFirewallApplicationFilter to create an object of all the rules.
+## Use the where object to find if there are any of our paths in the block rules.
+## If you find any, delete them. 
+## NOTES:
+    # -Get-NetFirewallRule allows us to return instances of firewall rules that matches the action "Allow".
+    # -Get-NetFirewallApplicationFilter accepts AssociatedNetFirewallRule <CimInstance> through the pipeline (Byvalue) and it returns objects filter objects based on the input.
+
+    try {
+        Write-Host "`n START: Section to delete current path rules with specific Action `n"
+        ## Use the ErrorAction Parameter to avoid catching the error and continue the search when no rules are found.
+        ## The ErrorVariable parameter is used to store the error in a custom name field which could be later 
+        $rulesWithAction = Get-NetFirewallRule -Action $actionToDelete -ErrorAction SilentlyContinue -ErrorVariable currentPathBlockSearchError
+        if ($rulesWithAction) {
+            ## Input object of Get-NetFirewallRule to Get-NetFirewallApplicationFilter to obtain properties based on Get-NetFirewallApplicationFilter
+            $applicationObject_Action = $rulesWithAction | Get-NetFirewallApplicationFilter
+            ## Iterate through each user folder and remove rules with respect to the user and the action
+            foreach ($user in $users) {
+                ## Combine the user folder path with path provided in the current location variable
+                $fullProgramPath = Join-Path -Path $user.FullName -ChildPath $currentPathLocation
+        
+                ## Return the filter objects for the program path that team is currently in.
+                $applicationObject_Program = $applicationobject_Action | Where-Object {$_.Program -eq $fullProgramPath}
+
+                if ($applicationObject_Program) {
+                    $applicationObject_Program | Remove-NetFirewallRule
+                    Write-Host "OUTPUT: Rule with path '$($fullProgramPath)' deleted"        
                 }
             }
         }
         else {
-            Write-Host "No Old Path Location"
-        }
-    }
-    catch {
-        Write-Host "`n START: Catch Section to delete old path rules `n "
-        Write-Host "Exception.Message [$($_.Exception.Message)]"
-        Write-Host "`n END: Catch Section to delete old path rules `n"
-    }
-    finally{
-        Clear-Variable fullProgramPath
-        Write-Host "`n END: Section to delete old path rules `n"
-    }
-    
-    
-    
-    
-    # Remove firewall rules with appropriate action and current Path
-    
-    # Get-NetFirewallRule allows us to return instances of firewall rules that matches the action "Allow".
-    # Get-NetFirewallApplicationFilter accepts AssociatedNetFirewallRule <CimInstance> through the pipeline (Byvalue) and it returns objects filter objects based on the input.
-    try {
-        Write-Host "`n START: Section to delete current path rules with specific Action `n"
-        if (Get-NetFirewallRule -Action $actionToDelete) {
-            # Iterate through each user folder and remove rules with respect to the user and the action
-            foreach ($user in $users) {
-                # Combine the user folder bath with path provided in the current location variable
-                $fullProgramPath = Join-Path -Path $user.FullName -ChildPath $currentPathLocation
-
-                $applicationObject_Action = Get-NetFirewallRule -Action $actionToDelete | Get-NetFirewallApplicationFilter
-        
-                # Return the filter objects for the program path that team is currently in.
-                $applicationObject_Program = $applicationobject_Action | Where-Object {$_.Program -eq $fullProgramPath}
-            
-                $applicationObject_Program | Remove-NetFirewallRule
-                Write-Host "Rule with path '$($fullProgramPath)' deleted"
-            }
-        }
-        else {
-            Write-Host "No firewall rules with give action to delete"
+            Write-Host "OUTPUT: No firewall rules with given action to delete"
         }
     }
     catch {
         Write-Host "`n START: Catch section to delete current path rules with specific Action `n"
         Write-Host "Exception.Message [$($_.Exception.Message)]"
+        Write-Host "Invocation Command [$($_.InvocationInfo.MyCommand.Name)]"
         Write-Host "`n END: Catch section to delete current path rules with specific Action `n"
     }
     finally{
-        Clear-Variable fullProgramPath
         Write-Host "`n END: Section to delete current path rules with specific Action `n"
     }
 
+## Section: Add the appropriate rules. 
+## How it works:
+## Check if the current path locations exist
+## Iterate through the progam field in the firewall in order to find deprecated paths.
+## Iterate through the users folder to create proper user paths.
+## Test if the path exists and then test if the paths exists. If yes, do not create the rule.
+## Else create the rule(s) 
 
-    # Add new rules to the firewall based on the current location path.
     try {
         Write-Host "`n BEGIN: Section to Add new rules based on the current path `n"
         if ($currentPathLocation) {
+            $counter = 0
             foreach($user in $users){
                 $fullProgramPath = Join-Path -Path $user.FullName -ChildPath $currentPathLocation
-                Write-Host "Full program path: $fullProgramPath"
                 if (Test-Path $fullProgramPath) {
                     if (-not (Get-NetFirewallApplicationFilter -Program $fullProgramPath -ErrorAction SilentlyContinue)) {
                         $ruleName = "Microsoft Teams for user $($user.Name)"
                         "UDP", "TCP" | ForEach-Object { New-NetFirewallRule -DisplayName $ruleName -Direction Inbound -Profile Domain -Program $fullProgramPath -Action $actionToAdd -Protocol $_ } | Out-Null
-                        Write-Host "Rule for $($ruleName) added"
+                        Write-Host "OUTPUT: Rule for $($ruleName) for the program path $($fullProgramPath) added"
+                        $counter = 1
                     }
                 }
-                else {
-                    Write-Host "Path does not exist"
-                }
-            }       
+            }
+            if ($counter -eq 0) {
+                Write-Host "OUTPUT: No Rule Added."
+            }
+        }
+        else {
+            Write-Host "OUTPUT: Path is empty"
         }
     }
     catch {
@@ -111,8 +160,7 @@ if ($null -ne $users) {
         Write-Host "`n END: Catch Section to Add new rules based on the current path `n"
     }
     finally{
-        Clear-Variable ruleName
-        Clear-Variable fullProgramPath
+        
         Write-Host "`n END: Section to Add new rules based on the current path `n"
 
     }
